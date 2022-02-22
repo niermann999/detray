c7c1446f76e910114353b7a7b635ab298350d370
Joana Niermann <joana.niermann@cern.ch>
2022-02-23 00:59:19 +0100

    fix constructor

--- tests/unit_tests/cuda/mask_store_cuda_kernel.cu
+++ tests/unit_tests/cuda/mask_store_cuda_kernel.cu
@@ -13,7 +13,7 @@ namespace detray {
 /// test kernel function to fill the output vector with is_inside function
 /// return values
 __global__ void mask_test_kernel(
-    mask_store_data<mask_store<thrust::tuple, dvector, darray, rectangle,
+    mask_store_data<mask_store<thrust::tuple, dvector, darray, 0, rectangle,
                                trapezoid, ring, cylinder, single, annulus>>
         store_data,
     vecmem::data::vector_view<point2> input_point2_data,
@@ -56,8 +56,7 @@ __global__ void mask_test_kernel(
 }
 
 void mask_test(
-    mask_store_data<mask_store<thrust::tuple, dvector, rectangle, trapezoid,
-                               ring, cylinder, single, annulus>>& store_data,
+    mask_store_data<mask_store<thrust::tuple, dvector, darray, 0, rectangle, trapezoid, ring, cylinder, single, annulus>>& store_data,
     vecmem::data::vector_view<point2>& input_point2_data,
     vecmem::data::vector_view<point3>& input_point3_data,
     vecmem::data::jagged_vector_view<intersection_status>& output_data) {
