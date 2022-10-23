81da3bc0a3b0e79c9cd1b600f69ea88ce3aec233
Joana Niermann <joana.niermann@cern.ch>
2022-10-23 08:39:19 +0200

    Update current benchmarks and add new CPU benchmarks

--- tests/benchmarks/cuda/benchmark_propagator_cuda.cpp
+++ tests/benchmarks/cuda/benchmark_propagator_cuda.cpp
@@ -29,19 +29,82 @@ vecmem::cuda::device_memory_resource dev_mr;
 vecmem::binary_page_memory_resource bp_mng_mr(mng_mr);
 
 // detector configuration
-auto toy_cfg =
-    toy_det_config{}.n_brl_layers(4u).n_edc_layers(7u).do_check(false);
+auto toy_cfg = toy_det_config{}
+                   .n_brl_layers(4u)
+                   .n_edc_layers(7u)
+                   .do_check(false)
+                   .use_material_maps(true);
 
 void fill_tracks(vecmem::vector<free_track_parameters<algebra_t>> &tracks,
                  const std::size_t theta_steps, const std::size_t phi_steps) {
-    // Set momentum of tracks
-    const scalar mom_mag{10.f * unit<scalar>::GeV};
+    using scalar_t = dscalar<algebra_t>;
+    using uniform_gen_t =
+        detail::random_numbers<scalar_t,
+                               std::uniform_real_distribution<scalar_t>>;
+    using track_generator_t =
+        random_track_generator<free_track_parameters<algebra_t>, uniform_gen_t>;
+
+    track_generator_t::configuration trk_cfg{};
+    trk_cfg.n_tracks(theta_steps * phi_steps)
+        .randomize_charge(true)
+        .eta_range(-4.f, 4.f)
+        .pT_range(1.f * unit<scalar_t>::GeV, 100.f * unit<scalar_t>::GeV);
 
     // Iterate through uniformly distributed momentum directions
-    for (auto traj : uniform_track_generator<free_track_parameters<algebra_t>>(
-             phi_steps, theta_steps, mom_mag)) {
+    for (auto traj : track_generator_t(trk_cfg)) {
         tracks.push_back(traj);
     }
+}
+
+template <propagate_option opt>
+static void BM_PROPAGATOR_CPU(benchmark::State &state) {
+
+    // Create the toy geometry and bfield
+    auto [det, names] = build_toy_detector(host_mr, toy_cfg);
+    test::vector3 B{0.f, 0.f, 2.f * unit<scalar>::T};
+    auto bfield = bfield::create_const_field(B);
+
+    // Create propagator
+    propagation::config cfg{};
+    cfg.navigation.search_window = {3u, 3u};
+    propagator_host_type p{cfg};
+
+    std::size_t total_tracks = 0;
+
+    // Get tracks
+    vecmem::vector<free_track_parameters<algebra_t>> tracks(&host_mr);
+    fill_tracks(tracks, static_cast<std::size_t>(state.range(0)),
+                static_cast<std::size_t>(state.range(0)));
+
+    total_tracks += tracks.size();
+
+    for (auto _ : state) {
+
+#pragma omp parallel for
+        for (auto &track : tracks) {
+
+            parameter_transporter<algebra_t>::state transporter_state{};
+            pointwise_material_interactor<algebra_t>::state interactor_state{};
+            parameter_resetter<algebra_t>::state resetter_state{};
+
+            auto actor_states =
+                tie(transporter_state, interactor_state, resetter_state);
+
+            // Create the propagator state
+            propagator_host_type::state p_state(track, bfield, det);
+
+            // Run propagation
+            if constexpr (opt == propagate_option::e_unsync) {
+                ::benchmark::DoNotOptimize(p.propagate(p_state, actor_states));
+            } else if constexpr (opt == propagate_option::e_sync) {
+                ::benchmark::DoNotOptimize(
+                    p.propagate_sync(p_state, actor_states));
+            }
+        }
+    }
+
+    state.counters["TracksPropagated"] = benchmark::Counter(
+        static_cast<double>(total_tracks), benchmark::Counter::kIsRate);
 }
 
 template <propagate_option opt>
@@ -60,18 +123,14 @@ static void BM_PROPAGATOR_CUDA(benchmark::State &state) {
 
     std::size_t total_tracks = 0;
 
+    // Get tracks
+    vecmem::vector<free_track_parameters<algebra_t>> tracks(&bp_mng_mr);
+    fill_tracks(tracks, static_cast<std::size_t>(state.range(0)),
+                static_cast<std::size_t>(state.range(0)));
+
+    total_tracks += tracks.size();
+
     for (auto _ : state) {
-
-        state.PauseTiming();
-
-        // Get tracks
-        vecmem::vector<free_track_parameters<algebra_t>> tracks(&bp_mng_mr);
-        fill_tracks(tracks, static_cast<std::size_t>(state.range(0)),
-                    static_cast<std::size_t>(state.range(0)));
-
-        total_tracks += tracks.size();
-
-        state.ResumeTiming();
 
         // Get tracks data
         auto tracks_data = vecmem::get_data(tracks);
